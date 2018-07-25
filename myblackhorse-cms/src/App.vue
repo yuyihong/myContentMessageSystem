483e1968480ff545919a5e4de87f8eb672a27eea
yyh <2199388991@qq.com>
2018-07-25 19:57:23 +0800

    新闻列表页完成

--- myblackhorse-cms/src/App.vue
+++ myblackhorse-cms/src/App.vue
@@ -1,39 +1,43 @@
 <template>
   <div id="app">
-    <mt-header fixed title="内容信息系统"></mt-header>
+    <mt-header fixed title="内容信息系统">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+    </mt-header>
 
     <router-view></router-view>
     <nav class="mui-bar mui-bar-tab">
-      <a class="mui-tab-item mui-active" href="#tabbar">
+      <router-link class="mui-tab-item mui-active" to="/">
         <span class="mui-icon mui-icon-home"></span>
         <span class="mui-tab-label">首页</span>
-      </a>
-      <a class="mui-tab-item" href="#tabbar-with-chat">
+      </router-link>
+      <router-link class="mui-tab-item" to="/member">
         <span class="mui-icon mui-icon-contact">
-          
+
         </span>
         <span class="mui-tab-label">会员</span>
-      </a>
-      <a class="mui-tab-item" href="#tabbar-with-contact">
+      </router-link>
+      <router-link class="mui-tab-item" to="/cart">
         <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
           <span class="mui-badge">0</span>
         </span>
         <span class="mui-tab-label">购物车</span>
-      </a>
-      <a class="mui-tab-item" href="#tabbar-with-map">
+      </router-link>
+      <router-link class="mui-tab-item" to="/search">
         <span class="mui-icon mui-icon-search"></span>
         <span class="mui-tab-label">搜索</span>
-      </a>
+      </router-link>
     </nav>
   </div>
 </template>
 
 <style lang="less">
-  .page {
-    margin-top: 40px;
-  }
+.page {
+  margin-top: 40px;
+}
 
-  body {
-    background-color: #fff !important;
-  }
+body {
+  background-color: #fff !important;
+}
 </style>
--- myblackhorse-cms/src/views/news/List.vue
+++ myblackhorse-cms/src/views/news/List.vue
@@ -1,9 +1,35 @@
 <template>
-    <div id="page">
-        <ul>
-            <li>
-                
+    <div class="page">
+        <ul class="mui-table-view mui-table-view-chevron">
+            <li v-for="(item,index) in newsList" :key="index" class="mui-table-view-cell mui-media">
+                <router-link :to="'./news/detail?id=' + item.id" class="mui-navigate-right">
+                    <img class="mui-media-object mui-pull-left" :src="item.img_url">
+                    <div class="mui-media-body mui-ellipsis">
+                        {{item.title}}
+                        <p class="mui-ellipsis">{{item.zhaiyao}}</p>
+                    </div>
+                </router-link>
             </li>
         </ul>
     </div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return {
+            newsList:[]
+        }
+    },
+    created(){
+        axios({
+            url:"http://www.liulongbin.top:3005/api/getnewslist"           
+        }).then(res => {
+            // console.log(res.data.message);
+            this.newsList = res.data.message;
+        })
+    }
+}
+</script>
+

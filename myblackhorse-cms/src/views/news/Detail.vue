6e83195fe114a9a337673ed7c64ad16b53b2f81a
yyh <2199388991@qq.com>
2018-07-25 21:08:58 +0800

    新闻详情页上半部分完成

--- myblackhorse-cms/src/views/news/Detail.vue
+++ myblackhorse-cms/src/views/news/Detail.vue
@@ -1,5 +1,42 @@
 <template>
-    <div id="page">
-        新闻详情
+    <div class="page news">
+        <div class="news-detail">
+            <div class="news-title">
+                <h4>{{newsInfo.title}}</h4>
+                <p class="news-info">
+                    <span>发表时间:{{newsInfo.add_time}}</span>
+                    <span class="mui-pull-right">点击： {{newsInfo.click}}次</span>
+                </p> <hr></div>
+            <div class="news-content" v-html="newsInfo.content">
+
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      newsInfo: {}
+    };
+  },
+  created() {
+    axios({
+      //获取传入的id
+      //
+      url: "http://www.liulongbin.top:3005/api/getnew/" + this.$route.query.id
+    }).then(res => {
+      console.log(res.data.message);
+      this.newsInfo = res.data.message[0];
+    });
+  }
+};
+</script>
+
+<style>
+.news {
+  padding: 5px;
+}
+</style>
--- myblackhorse-cms/src/views/news/List.vue
+++ myblackhorse-cms/src/views/news/List.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="page">
+    <div class="page news">
         <ul class="mui-table-view mui-table-view-chevron">
             <li v-for="(item,index) in newsList" :key="index" class="mui-table-view-cell mui-media">
-                <router-link :to="'./news/detail?id=' + item.id" class="mui-navigate-right">
+                <router-link :to="'/news/detail?id=' + item.id" class="mui-navigate-right">
+                <!-- 不能加个点 -->
                     <img class="mui-media-object mui-pull-left" :src="item.img_url">
                     <div class="mui-media-body mui-ellipsis">
                         {{item.title}}
@@ -32,4 +33,10 @@ export default {
     }
 }
 </script>
+
+<style>
+    .news {
+        padding: 5px;
+    }
+</style>
 
